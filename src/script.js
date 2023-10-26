a95b65ada0f3ed0855149afccfcab99d1873c1df
Ryan <muhamadrynsyy270521@gmail.com>
2023-10-27 01:01:30 +0700

    Menambah tampilan mobile dan lainnya

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 // Hamburger menu
-const hamburger = document.querySelector("#hamburger");
+const hamburger = document.querySelector(".hamburger-menu");
 const navMenu = document.querySelector("#nav-menu");
 
 hamburger.addEventListener("click", function () {
@@ -14,3 +14,31 @@ window.addEventListener("click", function (e) {
     navMenu.classList.add("dNone");
   }
 });
+
+function validate() {
+  let name = document.querySelector("#name");
+  let email = document.querySelector("#email");
+  let pesan = document.querySelector("#pesan");
+  let btnKirim = document.querySelector(".btn-kirim");
+
+  btnKirim.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    if (name.value == "" || email.value == "" || pesan.value == "") {
+      inputempty();
+    } else {
+      sendmail(name.value, email.value, pesan.value);
+
+      console.log("Pesan berhasil terkirim");
+    }
+  });
+}
+validate();
+
+function sendmail(name, email, pesan) {
+  emailjs.send("service_9o4p2pa","template_dd8ebfh", {
+    from_name: name,
+    to_name: email,
+    message: pesan,
+  });
+}
